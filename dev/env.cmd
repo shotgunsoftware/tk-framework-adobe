bc6962b430ae4d413a9a8d8af6eeeda01899e2a5
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-09-20 00:24:50 -0500

    fix adobe af

--- dev/env.cmd
+++ dev/env.cmd
@@ -3,24 +3,24 @@ rem set this to the target version you want to set this to
 set "TARGET_VERSION=0.0.1"
 
 rem set this to the core you are working with. eg:0.18.160
-set "TKCORE_VERSION=0.18.160"
+rem set "TKCORE_VERSION=0.18.160"
 rem if you want to use tk-core from a non default bundle-cache location,
 rem uncomment the following line and set your path accordingly.
-rem set "TKCORE_FOLDER="
+set "TKCORE_FOLDER=C:\Users\chaucae\Documents\projects\tk-core"
 
-set "ZXP_SIGN_TOOL="
+set "ZXP_SIGN_TOOL=C:\Users\chaucae\Documents\projects\CEP-Resources\ZXPSignCMD\4.1.1\win64\ZXPSignCmd.exe"
 set "PYTHON_EXE=python"
 
 rem DO NOT COMMIT THE FOLLOWING LINES TO THE REPO
 rem set this to your certificate file, that you created / will create
-set "CERTIFICATE_FILE="
+set "CERTIFICATE_FILE=C:\Users\chaucae\cert.p12"
 rem set this to the password that you chose/want to chose for your certificate file
 rem the following options are needed, when you want to create a new certificate
-set "CERTIFICATE_PASS="
-set "CERT_COUNTRY="
-set "CERT_STATE="
-set "CERT_ORG="
-set "CERT_CN="
+set "CERTIFICATE_PASS=zxcvbnm"
+set "CERT_COUNTRY=Peru"
+set "CERT_STATE=Arequipa"
+set "CERT_ORG=Autodesk"
+set "CERT_CN=Arequipa"
 
 rem optional certificate fields (uncomment if desired)
 set "CERT_LOCALITY="
